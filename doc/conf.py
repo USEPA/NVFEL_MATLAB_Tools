ab52ec4d47386e98c2083591544b1c3da516b4ea
kevinanewman <newman.kevin@epa.gov>
2021-01-19 11:32:18 -0500

    updates

--- doc/conf.py
+++ doc/conf.py
@@ -25,8 +25,8 @@ copyright = '2020, EPA'
 author = 'EPA'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
-version = '0.0.1'
+release = '0.1.0'
+version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
